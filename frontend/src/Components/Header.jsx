7fbb31ed249a0eeb9f02e72f74932fa97d4b9014
NencyTaraviya21 <nencytaraviya2102@gmail.com>
2024-09-18 00:33:51 +0530

    edited frontend

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,16 +1,10 @@
-import './header.scss'
+import './header.css'
+// import './index.css';
+
 const Header = () => {
   return (
     <div>
-        <nav className="header">
-            <div className="header__logo">
-                <h1>TASK MANAGER</h1>
-            </div>
-            <div className="header__buttons">
-                <button>Sign-in</button>
-                <button>Sign-out</button>
-            </div>
-        </nav>
+       
     </div>
   )
 }
